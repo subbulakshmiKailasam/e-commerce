be19b54af11b9fc6d11e0dbb41c396915e80a476
subbulakshmik <subbukeerthi96@gmail.com>
2024-09-10 11:06:51 +0530

    swagger

--- node/app.ts
+++ node/app.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import bodyparser from 'body-parser'
 import { connect } from './connection'
 import { userrouter } from './src/routes/user.router';
+import swaggerDocument from './swagger.json';
+import swaggerUi from 'swagger-ui-express';
 
-connect();
+
+//connect();
 const app = express();
 const port = 3000;
 app.get('/api/', (req, res) => {
   res.send('test');
 });
+
 app.use(bodyparser.urlencoded({ extended: false }))
 
 app.use(function (req, res, next) {
@@ -16,9 +20,9 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 app.use(bodyparser.json())
 app.use('/user', userrouter)
+app.get('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/api/test', (req, res) => {
   console.log(req.body)
   res.send('test');
